14363523bc42b4010b6b4d63006436f3f2faa695
SnrNotHere16 <aquineda16@hotmail.com>
2020-07-18 20:25:47 -0700

    Update initGPIO.c

--- TM4C123GXL_Project/initGPIO.c
+++ TM4C123GXL_Project/initGPIO.c
@@ -1,7 +1,7 @@
 /* 
 Definitions for the GPIO pins functions for the Tic Tac Toe game which includes 
 	1. 18 outputs  (LED) (PB0-PB7, PA4), (PD2-PD3, PD6-PD7,  PC5-PC7, PE0,PE3)
-	2. 3 inputs (Buttons) (PF0,PF4)
+	2. 3 inputs (Buttons) (PF0,PF4, PE5)
 	3. 2 inputs (ADC) (PE1-PE2)
 	4. 2 outputs (PWM) (PC4,PE4)
 */ 
